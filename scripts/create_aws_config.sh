447634e38e015bfb9d9b868fb9215697cc09b109
Chris Barker <github@mrz.angrydome.com>
2023-05-19 17:41:24 +0100

    Update create_aws_config.sh

--- scripts/create_aws_config.sh
+++ scripts/create_aws_config.sh
@@ -10,6 +10,6 @@ sso_start_url = ${AWS_SSO_URL}
 sso_region = ${AWS_SSO_REGION}
 sso_account_id = ${AWS_ACCOUNT_ID}
 sso_role_name = ${AWS_ROLE_NAME}
-region = eu-west-1
+region = ${AWS_REGION}
 AWSFILE
 
