e7a64b5de2485dd96f471982c786b10c3ffef42e
Chris Barker <github@mrz.angrydome.com>
2024-03-20 14:26:02 +0000

    make things basic for now

--- setup.sh
+++ setup.sh
@@ -6,9 +6,13 @@ CURRENT=$PWD
 
 cd $TMPDIR
 
-for script in ~/.dotfiles/scripts/*; do
-  bash "$script"
-done
+# for script in ~/.dotfiles/scripts/*; do
+#   bash "$script"
+# done
+
+bash "~/.dotfiles/scripts/prep_gpg.sh"
+
+bash "~/.dotfiles/scripts/shellcheck.sh"
 
 cd $CURRENT
 
